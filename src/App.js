37640d11dd64e51d2923dff4dd65e58f9358338b
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-10-17 14:57:14 +0200

    add react router, and example paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import UserList from './components/UserList/UserList';
+import CreateUser from './components/CreateUser/CreateUser';
+import EditUser from './components/EditUser/EditUser';
+
 function App() {
   return (
     <div className='App'>
-      <p>test</p>
+      <h5>React CRUD operations using PHP API and MySQL</h5>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/'>User List</NavLink>
+            </li>
+            <li>
+              <NavLink to='user/create'>Create User</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<UserList />} />
+          <Route path='user/create' element={<CreateUser />} />
+          <Route path='user/:id/edit' element={<EditUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
