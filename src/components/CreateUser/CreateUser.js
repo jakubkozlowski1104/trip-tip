08c1c8ab8c11150becb79bf7098a9e6f4f0eb149
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-10-17 19:15:38 +0200

    create form and inputs

--- src/components/CreateUser/CreateUser.js
+++ src/components/CreateUser/CreateUser.js
@@ -1,7 +1,38 @@
+import { useState } from 'react';
+
 const CreateUser = () => {
-    return ( 
-        <h1>Create User</h1>
-     );
-}
- 
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+  return (
+    <>
+      <h1>Create User</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='form-input'>
+          <label>Email:</label>
+          <input type='text' name='email' onChange={handleChange} />
+        </div>
+        <div className='form-input'>
+          <label>Name:</label>
+          <input type='text' name='name' onChange={handleChange} />
+        </div>
+        <div className='form-input'>
+          <label>Password:</label>
+          <input type='password' name='password' onChange={handleChange} />
+        </div>
+        <button>Create</button>
+      </form>
+    </>
+  );
+};
+
 export default CreateUser;
