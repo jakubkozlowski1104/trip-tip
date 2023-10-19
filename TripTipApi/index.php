9724a1dfdfbb4a9cdba14e1743740fed73bdc14a
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-10-19 11:41:27 +0200

    map users

--- TripTipApi/index.php
+++ TripTipApi/index.php
@@ -12,29 +12,35 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 
-echo "Test";
-
 include 'DbConnect.php';
 $objDb = new DbConnect();
 $conn = $objDb->connect();
 
 $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
+    case "GET":
+        $sql = "SELECT * FROM users";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        header('Content-Type: application/json');  // Set the Content-Type header
+        echo json_encode($users);
+        break;
     case "POST":
-            $user = json_decode(file_get_contents('php://input'));
-            $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':name', $user->name);
-            $stmt->bindParam(':email', $user->email);
-            $stmt->bindParam(':password', $user->password);
-            if ($stmt->execute()) {
-                $response = ['status' => 1, 'message' => 'Record created successfully'];
-            } else {
-                $response = ['status' => 0, 'message' => 'Failed to create record'];
-            }
-            echo json_encode($response);
-            break;
-}
+        $user = json_decode(file_get_contents('php://input'));
+        $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':name', $user->name);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':password', $user->password);
+        if ($stmt->execute()) {
+            $response = ['status' => 1, 'message' => 'Record created successfully'];
+        } else {
+            $response = ['status' => 0, 'message' => 'Failed to create record'];
+        }
+        echo json_encode($response);
+        break;
+    }
 ?>
 
 
--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,20 +1,34 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = () => {
+    axios.get('http://localhost/TripTipApi/users/').then((response) => {
+      setUsers(response.data);
+    });
+  };
+
   useEffect(() => {
     getUsers();
   }, []);
 
-  const getUsers = () => {
-    axios.get('http://localhost/TripTipApi/users').then((response) => {
-      console.log(response.data);
-    });
-  };
-
   return (
     <>
       <h1>User List</h1>
+      <ul>
+        {users.map((user, id) => (
+          <li key={user.id}>
+            <p>Id: {id + 1}</p>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <NavLink to={`user/${user.id}/edit`}>Edit </NavLink>
+            <button>deltete</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
