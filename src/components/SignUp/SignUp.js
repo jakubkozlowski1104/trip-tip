3dd78713108237fec6ba482bb80570c1ba10a410
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-10-22 22:06:24 +0200

    copy Login

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,18 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { StyledLogin, StyledCenter, StyledForm } from '../SignUp/SignUp.styles';
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleExclamation,
+  faLock,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'; // Import the specific icon
 
 const SignUp = () => {
   const [inputs, setInputs] = useState({});
+  const [isLogIn, setIsLogIn] = useState(false);
+  const [isloginwrong, setIsLoginWrong] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -22,25 +31,64 @@ const SignUp = () => {
     navigate('/');
   };
 
+  const changePath = () => {
+    if (isLogIn) {
+      console.log('siema');
+      navigate('/user/home');
+    }
+  };
+
   return (
-    <>
-      <h1>Create User</h1>
-      <form onSubmit={handleSubmit}>
-        <div className='form-input'>
-          <label>Email:</label>
-          <input type='text' name='email' onChange={handleChange} />
-        </div>
-        <div className='form-input'>
-          <label>Name:</label>
-          <input type='text' name='name' onChange={handleChange} />
-        </div>
-        <div className='form-input'>
-          <label>Password:</label>
-          <input type='password' name='password' onChange={handleChange} />
-        </div>
-        <button>Add user</button>
-      </form>
-    </>
+    <StyledCenter>
+      <StyledLogin isloginwrong={isloginwrong ? 'true' : undefined}>
+        <h1>Login</h1>
+        <StyledForm onSubmit={handleSubmit}>
+          <div className='form-input'>
+            <input
+              placeholder='Email'
+              type='text'
+              name='email'
+              onChange={handleChange}
+            />
+            <div className='icon'>
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+          </div>
+          <div className='form-input'>
+            <input
+              placeholder='Password'
+              type='password'
+              name='password'
+              onChange={handleChange}
+            />
+            <div className='icon'>
+              <FontAwesomeIcon icon={faLock} />
+            </div>
+          </div>
+          <div className='info'>
+            <div className='error'>
+              {isloginwrong && (
+                <>
+                  <FontAwesomeIcon
+                    className='error-icon'
+                    icon={faCircleExclamation}
+                  />
+                  <p>Entered data is incorrect</p>
+                </>
+              )}
+            </div>
+            <p className='forgot'>Forgot Password?</p>
+          </div>
+          <button onClick={changePath()}>Login</button>
+          <div className='register'>
+            <p>
+              Don't have an accont?
+              <span onClick={() => navigate('/user/signup')}> Register</span>
+            </p>
+          </div>
+        </StyledForm>
+      </StyledLogin>
+    </StyledCenter>
   );
 };
 
