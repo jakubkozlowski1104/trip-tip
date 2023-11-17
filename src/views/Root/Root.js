d83b12d3a9e66b1ccabb2a9bb6b527aec2453cfd
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-11-17 17:56:02 +0100

    move react router from root to navbar

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,43 +1,14 @@
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-import SignUp from '../../components/SignUp/SignUp';
-import LogIn from '../../components/LogIn/LogIn';
-import UserList from '../../components/UserList/UserList';
-import EditUser from '../../components/EditUser/EditUser';
-import HomePage from '../HomePage/HomePage';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../../assets/styles/GlobalStyle';
 import { theme } from '../../assets/styles/Theme';
+import Navbar from '../Navbar/Navbar';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <div className='App'>
-        <BrowserRouter>
-          <nav>
-            <ul>
-              <li>
-                <NavLink to='/'>User List</NavLink>
-              </li>
-              <li>
-                <NavLink to='/user/home'>Home</NavLink>
-              </li>
-              <li>
-                <NavLink to='user/signup'>Sign Up</NavLink>
-              </li>
-              <li>
-                <NavLink to='user/login'>Log In</NavLink>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route index element={<UserList />} />
-            <Route path='user/signup' element={<SignUp />} />
-            <Route path='user/login' element={<LogIn />} />
-            <Route path='user/home' element={<HomePage />} />
-            <Route path='user/:id/edit' element={<EditUser />} />
-          </Routes>
-        </BrowserRouter>
+        <Navbar />
       </div>
     </ThemeProvider>
   );
