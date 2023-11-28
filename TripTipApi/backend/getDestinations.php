cd10904c0e72413f0809373915904a208228c9c9
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-11-28 13:32:48 +0100

    add getDestinations.php

---
+++ TripTipApi/backend/getDestinations.php
@@ -0,0 +1,38 @@
+<?php
+    require_once 'config.php';
+
+    $conn = (new DbConnect())->connect();
+
+    $sql = "SELECT 
+        destinations.id AS destination_id,
+        destinations.title,
+        destinations.publish_date,
+        destinations.description,
+        destinations.likes,
+        destinations.saves,
+        destinations.image_path AS destination_image_path,
+        destinations.map_img,
+        destinations.map_link,
+        countries.name AS country_name,
+        countries.flag_path,
+        reviews.content AS review_content,
+        users.name AS user_name,
+        destination_categories.name AS category_name
+    FROM 
+        destinations
+    JOIN 
+        countries ON destinations.country = countries.id
+    LEFT JOIN 
+        reviews ON destinations.review = reviews.id
+    LEFT JOIN 
+        users ON reviews.user_id = users.id
+    LEFT JOIN 
+        destination_categories ON destinations.category_id = destination_categories.id";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $destinations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    header('Content-Type: application/json');
+    echo json_encode($destinations);
+?>
